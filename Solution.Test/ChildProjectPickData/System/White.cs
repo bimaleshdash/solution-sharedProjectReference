1143a166b40e7a8e420094b6028dc3d06708553d
bimaleshdash@outlook.com <bimaleshdash@outlook.com>
2020-08-17 16:38:40 -0400

    class library1 included - the child package

--- Solution.Test/ChildProjectPickData/System/White.cs
+++ Solution.Test/ChildProjectPickData/System/White.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using modernParent = ProjectReferredCommon;
-
+using ClassLibrary1;
 
 namespace ChildProjectPickData.System
 {
@@ -11,6 +11,8 @@ namespace ChildProjectPickData.System
         public void Checker()
         {
             var test = new modernParent.Collection.TollSTart("Yes", 5).TollGate();
+            var tes12t = new ClassLibrary1.Green();
+
         }
     }
 }
---
+++ Solution.Test/ProjectReferredCommon/Collection/Processor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectReferredCommon.Collection
+{
+    class Processor
+    {
+        void Microclass(String changes)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+
+            }
+
+        }
+    }
+}
